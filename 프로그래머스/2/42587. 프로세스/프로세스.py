bfd0f67a54d258b0cd47c1fbbac7672c89beb3fe
Cow-Kite <91773118+Cow-Kite@users.noreply.github.com>
2025-01-27 05:51:39 +0900

    [level 2] Title: 프로세스, Time: 0.10 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,15 +2,16 @@ from collections import deque
 
 def solution(priorities, location):
     cnt = 0
-    proc = deque(enumerate(priorities))  # (index, priority) 형태로 저장
-    sorted_priorities = sorted(priorities, reverse=True)  # 우선순위를 정렬하여 사용
+    proc = deque(enumerate(priorities))
+    sorted_prior = sorted(priorities, reverse=True)
     
     while proc:
-        idx, priority = proc.popleft()  # 가장 앞의 문서 가져오기
-        if priority == sorted_priorities[0]:  # 현재 문서의 우선순위가 가장 높은 경우
-            cnt += 1  # 실행된 문서 수 증가
-            sorted_priorities.pop(0)  # 실행된 문서 제거
-            if idx == location:  # 원하는 위치의 문서인지 확인
-                return cnt
+        idx, priority = proc.popleft()
+        if priority == sorted_prior[0]:
+            cnt += 1
+            sorted_prior.pop(0)
+            if idx == location:
+                return cnt 
         else:
-            proc.append((idx, priority))  # 다시 큐에 추가
+            proc.append((idx, priority))
+                
